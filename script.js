8df4b6cf77c4c3e21bfc2af95235062208b9244f
SashaH62 <sashahermanwebdev@gmail.com>
2022-07-26 11:12:54 +0200

    Add fetch request for Json data

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 "use strict";
 
 //Import Json file
-import weeklyCost from "./data.json" assert { type: "json" };
+// const response = await fetch(urlOrRequest[, options])
+// import weeklyCost from "./data.json" assert { type: "json" };
 
 // Assign all elements to variables
 const graphColumns = document.querySelectorAll(".graph-bar-section");
@@ -9,12 +10,12 @@ const graphBarsContainers = document.querySelectorAll(".bar-container");
 const graphBars = document.querySelectorAll(".bar");
 const costCards = document.querySelectorAll(".cost-display");
 
-let largestAmount = 0;
+let lgAmount = 0;
 
 // Calculate height based on percentage of highest amount and return percentage value out of 140px
 
-const calculateHeight = (dailyCost) => {
-  let percentage = Math.floor((dailyCost / largestAmount) * 100);
+const calculateHeight = (dailyCost, fn) => {
+  let percentage = Math.floor((dailyCost / fn) * 100);
   return (percentage / 100) * 140;
 };
 
@@ -24,23 +25,32 @@ const populateCostCard = (cost, costCard) => {
 
 // Find highest amount in Json file and add class to highest value
 
-for (let i = 0; i < weeklyCost.length; i++) {
-  if (largestAmount < weeklyCost[i].amount) {
-    largestAmount = weeklyCost[i].amount;
-    // Toggle class for largest amount
-    graphBars.forEach((bar) => bar.classList.remove("highest"));
-    graphBars[i].classList.add("highest");
+const findLargest = (weeklyCost) => {
+  let largestAmount = 0;
+  for (let i = 0; i < weeklyCost.length; i++) {
+    if (largestAmount < weeklyCost[i].amount) {
+      largestAmount = weeklyCost[i].amount;
+      lgAmount = largestAmount;
+      // Toggle class for largest amount
+      graphBars.forEach((bar) => bar.classList.remove("highest"));
+      graphBars[i].classList.add("highest");
+    }
   }
-}
+  return largestAmount;
+};
 
 // Apply calculated height to each column and populate cost cards
-for (let j = 0; j < weeklyCost.length; j++) {
-  let currentDailyCost = weeklyCost[j].amount;
-  graphBarsContainers[j].style.height = `${calculateHeight(
-    currentDailyCost
-  )}px`;
-  populateCostCard(weeklyCost[j].amount, costCards[j]);
-}
+
+const setHeight = (weeklyCost) => {
+  for (let j = 0; j < weeklyCost.length; j++) {
+    let currentDailyCost = weeklyCost[j].amount;
+    graphBarsContainers[j].style.height = `${calculateHeight(
+      currentDailyCost,
+      findLargest(weeklyCost)
+    )}px`;
+    populateCostCard(weeklyCost[j].amount, costCards[j]);
+  }
+};
 
 graphColumns.forEach((col) => {
   col.addEventListener("mouseenter", () => {
@@ -50,3 +60,13 @@ graphColumns.forEach((col) => {
     col.firstElementChild.style.opacity = "";
   });
 });
+
+fetch("/data.json", {
+  method: "GET",
+})
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (json) {
+    setHeight(json);
+  });
