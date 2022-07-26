4f0c19d60b7c4cb55ff30cdbf8dc131740e42770
SashaH62 <sashahermanwebdev@gmail.com>
2022-07-26 08:49:23 +0200

    Add media queries

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 "use strict";
 
+//Import Json file
 import weeklyCost from "./data.json" assert { type: "json" };
 
+// Assign all elements to variables
 const graphColumns = document.querySelectorAll(".graph-bar-section");
 const graphBarsContainers = document.querySelectorAll(".bar-container");
 const graphBars = document.querySelectorAll(".bar");
@@ -9,23 +11,29 @@ const costCards = document.querySelectorAll(".cost-display");
 
 let largestAmount = 0;
 
+// Calculate height based on percentage of highest amount and return percentage value out of 140px
+
 const calculateHeight = (dailyCost) => {
   let percentage = Math.floor((dailyCost / largestAmount) * 100);
-  return (percentage / 100) * 175;
+  return (percentage / 100) * 140;
 };
 
 const populateCostCard = (cost, costCard) => {
   costCard.innerHTML = `$${cost}`;
 };
 
+// Find highest amount in Json file and add class to highest value
+
 for (let i = 0; i < weeklyCost.length; i++) {
   if (largestAmount < weeklyCost[i].amount) {
     largestAmount = weeklyCost[i].amount;
+    // Toggle class for largest amount
     graphBars.forEach((bar) => bar.classList.remove("highest"));
     graphBars[i].classList.add("highest");
   }
 }
 
+// Apply calculated height to each column and populate cost cards
 for (let j = 0; j < weeklyCost.length; j++) {
   let currentDailyCost = weeklyCost[j].amount;
   graphBarsContainers[j].style.height = `${calculateHeight(
